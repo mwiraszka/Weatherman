b79839f2962e1b96d6209261c5af564e4d0d943c
Michal Wiraszka <michal@wiraszka.com>
2021-03-08 21:44:02 -0500

    basic layout with hard-coded values

---
+++ src/app/current-weather/current-weather.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+import { ICurrentWeather } from '../interfaces';
+
+@Component({
+  selector: 'app-current-weather',
+  templateUrl: './current-weather.component.html',
+  styleUrls: ['./current-weather.component.css']
+})
+export class CurrentWeatherComponent implements OnInit {
+  current: ICurrentWeather;
+  constructor() {
+    this.current = {
+      city: 'London',
+      country: 'Canada',
+      date: new Date(),
+      image: 'assets/img/sunny.svg',
+      temperature: 15,
+      description: 'sunny',
+    } as ICurrentWeather;
+  };
+  ngOnInit(): void {};
+}
