2fdffe134f2d25589dd033598025cd6fd3d0eb05
Michal Wiraszka <michal@wiraszka.com>
2021-03-17 22:54:43 -0400

    e2e test amended to reflect h1 tag change to mat-toolbar span tag

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -25,6 +25,6 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Weatherman');
+    expect(compiled.querySelector('span').textContent).toContain('Weatherman');
   });
 });
