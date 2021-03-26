78b04ff21df1b06e6426e7696779c1295af78b26
Michal Wiraszka <michal@wiraszka.com>
2021-03-26 13:56:40 -0400

    build environments swapped (test)

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,15 @@
+// export const environment = {
+//   production: true,
+//   appId: '2afa8f07bddea63ef2efbe6bf12fe17d',
+//   baseUrl: 'https://',
+//   username: 'weatherman91',
+//   geonamesApi: 'secure'
+// };
+
 export const environment = {
-  production: true,
+  production: false,
   appId: '2afa8f07bddea63ef2efbe6bf12fe17d',
-  baseUrl: 'https://',
+  baseUrl: 'http://',
   username: 'weatherman91',
-  geonamesApi: 'secure'
+  geonamesApi: 'api'
 };
